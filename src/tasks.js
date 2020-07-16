332d9370b53a673a4969aec4e4621f2dc9ad1f07
Ryan Speese <ryanspeese@gmail.com>
2020-07-16 15:35:09 -0500

    click on task title to show the description

--- src/tasks.js
+++ src/tasks.js
@@ -8,7 +8,7 @@ class Task {
 }
 
 const TaskModule = (function () {
-    const makeTask = function() {
+    const makeTask = function () {
         let desc = "";
         let dueDate = "";
         let priority = "";
@@ -40,10 +40,10 @@ const TaskModule = (function () {
             addTaskDiv(newTask);
             return 1;
         }
-        
+
     };
 
-    const addTaskDiv = function(task) {
+    const addTaskDiv = function (task) {
         let grid = document.getElementById('tasks-grid');
         let addBtn = document.getElementById('tasks-add');
         let taskDiv = document.createElement('div');
@@ -51,67 +51,92 @@ const TaskModule = (function () {
         let taskDate = document.createElement('span');
         let taskTitle = document.createElement('span');
         let taskCheck = document.createElement('input');
+        let taskDesc = document.createElement('p');
         let projName = document.querySelector('.active-project').childNodes[1].textContent;
 
         taskDiv.classList.add('tasks');
         taskDiv.classList.add(projName);
         taskCheck.type = "checkbox";
         taskCheck.class = "task-check";
-        taskCheck.classList.add("task-check"); 
+        taskCheck.classList.add("task-check");
         taskTitle.classList.add('task-title');
         taskDate.classList.add('task-date');
         taskImg.classList.add('del-task');
         taskImg.src = "https://img.icons8.com/material/24/000000/multiply--v1.png";
-        if (task.priority == "high"){
+        if (task.priority == "high") {
             taskDiv.classList.add('form-high');
         } else if (task.priority == "low") {
             taskDiv.classList.add('form-low');
         }
-        
+
         taskDiv.appendChild(taskCheck);
         taskDiv.appendChild(taskTitle);
         taskDiv.appendChild(taskDate);
         taskDiv.appendChild(taskImg);
-       
+
         taskTitle.innerHTML = task.title;
         taskDate.innerHTML = task.dueDate;
+        taskDesc.innerHTML = task.desc;
 
         grid.insertBefore(taskDiv, addBtn);
 
+
         addDeleteTask(taskImg);
         addCompleteTask(taskCheck);
+        addShowDescription(taskTitle, taskDesc);
     };
 
-    const addCompleteTask = function(check){
+    const addCompleteTask = function (check) {
         let tasksGrid = document.getElementById('tasks-grid');
         let compGrid = document.getElementById('completed-grid');
         let addBtn = document.getElementById('tasks-add');
 
-        check.addEventListener('click', function(){
+        check.addEventListener('click', function () {
             let taskDiv = check.parentNode;
-            if(check.checked == true){
+            if (check.checked == true) {
                 tasksGrid.removeChild(taskDiv);
                 compGrid.appendChild(taskDiv);
-            } else{
+            } else {
                 compGrid.removeChild(taskDiv);
                 tasksGrid.insertBefore(taskDiv, addBtn);
             }
-            
+
         })
-        
+
     }
 
-    const addDeleteTask = function(img){
-            img.addEventListener('click', function() {
-                let taskDiv = img.parentNode;
-                let grid = taskDiv.parentNode;
+    const addDeleteTask = function (img) {
+        img.addEventListener('click', function () {
+            let taskDiv = img.parentNode;
+            let grid = taskDiv.parentNode;
 
-                grid.removeChild(taskDiv);
-            })
-        }
+            grid.removeChild(taskDiv);
+        })
+    }
+
+    const addShowDescription = function (title, desc) {
+
+        title.addEventListener('click', function () {
+            let modal = document.querySelector('#modal');
+            let modalClose = modal.childNodes[1].childNodes[1];
+            let descPara = modal.childNodes[1].childNodes[5];
+
+            modal.style.display = "block";
+            descPara.textContent = desc.innerHTML;
+
+            modalClose.onclick = function () {
+                modal.style.display = 'none';
+            }
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            }
+        })
+    }
 
     return {
-        makeTask, 
+        makeTask,
     }
 })();
 
